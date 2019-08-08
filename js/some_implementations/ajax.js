0ab4e4d96c1696f3dc0b1bb9d7e92fc3415967c6
linwei <571361206@qq.com>
2019-08-08 13:43:24 +0800

    ajax

---
+++ js/some_implementations/ajax.js
@@ -0,0 +1,10 @@
+var xhr = new XMLHttpRequest()
+xhr.open('GET', '/api', true)
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+            console.log(xhr.responseText)
+        }
+    }
+}
+xhr(null)

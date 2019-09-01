1cb880ccb6a4a7fdca528bc7f553dcbb28a03c02
linwei <571361206@qq.com>
2019-09-01 11:12:56 +0800

    一些练习

--- js/some_implementations/ajax.js
+++ js/some_implementations/ajax.js
@@ -8,3 +8,14 @@ xhr.onreadystatechange = function () {
     }
 }
 xhr(null)
+
+var xhr = new XMLHttpRequest()
+xhr.open('GET','./api',true);
+xhr.onreadystatechange = function() {
+    if(xhr.readyState == 4) {
+        if(xhr.status == 200) {
+            console.log(xhr.responseText)
+        }
+    }
+}
+xhr(null)
